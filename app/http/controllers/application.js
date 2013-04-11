a01703f962549942ce1e86d012f21c635ad0c197
ksedge <kieran.sedgwick@gmail.com>
2013-04-11 12:23:42 -0400

    Restricted cross-origin requests to the contents of process.env.ALLOWED_DOMAINS

--- app/http/controllers/application.js
+++ app/http/controllers/application.js
@@ -3,8 +3,7 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 exports.allowCorsRequests = function(req, resp, next){
-  // TODO: Keep a configured list of allowed origins
-  resp.header('Access-Control-Allow-Origin', '*');
+  resp.header('Access-Control-Allow-Origin', process.env.ALLOWED_DOMAINS);
   resp.header('Access-Control-Allow-Methods', 'POST');
   // Access-Control-Allow-Headers
   next();
