d073954371d2eace538808976d486f70a5595cac
Ross Bruniges <rossbruniges@gmail.com>
2013-04-29 19:48:28 +0100

    [bug-866637] Using the AUDIENCE environment variable for our packaged dev app

--- app/dev/servers.js
+++ app/dev/servers.js
@@ -4,46 +4,30 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var express = require('express');
+var express = require( "express" ),
+    env = require( "../../config/environment" );
 
 var app1 = express();
 var app2 = express();
  
 // Configuration
-function configure(app){
-  app.configure(function(){
-    // app.use(express.bodyParser());
-    // app.use(express.methodOverride());
-    // // app.use(app.router);
-    app.use(express.static(__dirname + '/public'));
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+function configure( app ){
+  app.configure( function(){
+    app.set( "views", __dirname + "/views" );
+    app.set( "view engine", "ejs" );
+    app.use( app.router );
+    app.use( express.errorHandler( { dumpExceptions: true, showStack: true } ) );
+  });
+  app.get( "/", function( req, res ) {
+    res.render( "index", {
+        audience: env.get( "AUDIENCE" ),
+        allowed_domains: env.get( "ALLOWED_DOMAINS" ).split( " " )
+    });
   });
 }
 
-configure(app1);
-configure(app2);
+configure( app1 );
+configure( app2 );
 
-app1.listen(3001);
-app2.listen(3002);
-
-
-// Routes
-// app.get('/', function(req, res){
-//   res.render('index', {
-//     title: 'Home'
-//   });
-// });
-
-// app.get('/about', function(req, res){
-//   res.render('about', {
-//     title: 'About'
-//   });
-// });
-
-// app.get('/contact', function(req, res){
-//   res.render('contact', {
-//     title: 'Contact'
-//   });
-// });
-
-// Only listen on $ node app.js
+app1.listen( 3001 );
+app2.listen( 3002 );
