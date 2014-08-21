b414a5ca628c1d4a2e7a420729b45577bc366830
Christopher De Cairos <cade@mozillafoundation.org>
2014-08-21 10:53:52 -0400

    DB migration script

---
+++ migrations/new-login-migration.sql
@@ -0,0 +1,42 @@
+START TRANSACTION;
+
+  -- Create LoginTokens Table
+  CREATE TABLE `LoginTokens` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `token` varchar(5) DEFAULT NULL,
+    `failedAttempts` int(11) NOT NULL,
+    `used` tinyint(1) NOT NULL,
+    `createdAt` datetime NOT NULL,
+    `updatedAt` datetime NOT NULL,
+    `UserId` int(11) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+  ) ENGINE=InnoDB
+
+  -- Create Passwords Table
+  CREATE TABLE `Passwords` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `saltedHash` varchar(60) DEFAULT NULL,
+    `createdAt` datetime NOT NULL,
+    `updatedAt` datetime NOT NULL,
+    `UserId` int(11) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+  ) ENGINE=InnoDB
+
+  -- Create ResetAuthorizations Table
+  CREATE TABLE `ResetAuthorizations` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `token` varchar(32) NOT NULL,
+    `used` tinyint(1) NOT NULL DEFAULT '0',
+    `createdAt` datetime NOT NULL,
+    `updatedAt` datetime NOT NULL,
+    `UserId` int(11) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+  ) ENGINE=InnoDB
+
+  -- Add verified Column
+  ALTER TABLE `Users` ADD COLUMN verified TINYINT(1) NOT NULL DEFAULT 0;
+
+  -- Add usePasswordLogin Column
+  ALTER TABLE `Users` ADD COLUMN usePasswordLogin TINYINT(1) NOT NULL DEFAULT 0;
+
+COMMIT;
