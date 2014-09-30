276b3783ccb1d310e38e5025649e7355a05818dc
Kate Hudson <k88hudson@gmail.com>
2014-09-30 18:16:50 -0400

    review fix

--- app/http/cors.js
+++ app/http/cors.js
@@ -2,9 +2,9 @@ module.exports = function (allowedDomains) {
   return function cors(req, res, next) {
     if (allowedDomains.indexOf(req.headers.origin) > -1) {
       res.header('Access-Control-Allow-Origin', req.headers.origin);
-      res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE');
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-CSRF-Token, Accept-Ranges, Range-Unit, Content-Range, Range');
-      res.header('Access-Control-Expose-Headers', 'Content-Type, Accept-Ranges, Range-Unit, Content-Range');
+      res.header('Access-Control-Allow-Methods', 'POST');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, X-CSRF-Token');
+      res.header('Access-Control-Expose-Headers', 'Content-Type');
       res.header('Access-Control-Allow-Credentials', true);
     }
     next();
