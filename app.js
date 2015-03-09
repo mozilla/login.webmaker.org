ced7a2ae303a51b3f0338333a396cecac4fb1413
Christopher De Cairos <chris@chrisdecairos.ca>
2015-03-09 15:58:53 -0400

    requireable server module

--- app.js
+++ app.js
@@ -2,4 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-require("./app/http/server.js");
+var env = require("./config/environment");
+
+
+require("./app/http/server.js")(env);
